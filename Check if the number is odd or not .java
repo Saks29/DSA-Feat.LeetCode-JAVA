4350b337e889513bcf4913b263dbaa65d95d6882
Saksham Patwal <135833794+Saks29@users.noreply.github.com>
2025-09-12 19:07:32 +0530

    Create Check if the number is odd or not .java

---
+++ Check if the number is odd or not .java
@@ -0,0 +1,10 @@
+//Q.Given a positive integer n, determine whether it is odd or even. Return true if the number is even and false if the number is odd.
+class Solution {
+    static boolean isEven(int n) {
+        // code here
+        if(n%2 == 0){
+            return true;
+        }
+        return false;
+    }
+}
